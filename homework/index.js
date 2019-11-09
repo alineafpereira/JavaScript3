95bc6e82cd54aec64e72d2443e6babdfb1f56330
alineafpereira <aline.amaralds@gmail.com>
2019-11-09 23:51:57 +0100

    js3-week2-homework-aline-peirea
    not finished

--- homework/index.js
+++ homework/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const options = document.getElementById("select");
+
+
 {
   function fetchJSON(url, cb) {
     const xhr = new XMLHttpRequest();
@@ -18,10 +21,13 @@
 
   function createAndAppend(name, parent, options = {}) {
     const elem = document.createElement(name);
+    const nullValue = "no information in this topic"; /*created this condition so when this key comes empty, the default quote goes to the table */
     parent.appendChild(elem);
     Object.entries(options).forEach(([key, value]) => {
       if (key === 'text') {
         elem.textContent = value;
+      }if (value === null) {
+        elem.textContent = nullValue;
       } else {
         elem.setAttribute(key, value);
       }
@@ -29,11 +35,26 @@
     return elem;
   }
 
-  function renderRepoDetails(repo, ul) {
-    createAndAppend('li', ul, { text: repo.name });
+  function renderRepoDetails(repo, ul) { 
+    const menuOption = createAndAppend('option', options, {text: repo.name}) //here the repo's name goes to the list of option inside select tag//
+    menuOption.addEventListener('select', () => {
+      const table = createAndAppend('table', ul);
+      table.className = "lists";
+      const line1 = createAndAppend('tr', table)
+      line1.className = "line1"
+      createAndAppend('td', line1, {text: "Name:"})
+      
+    }) 
   }
+  /*Up here I tried to create a really basic structure to an addEventListener. My intention was have something that,
+   when I select the name of the project it goes to my table. After I had this working I would create the two boxes as the examples shows.
+  But is not working. I spent lots of time reviewing the content so I could learn better to modify the project but  was not enough.
+  I was hoping I could advance so much more than this but here I am stucked again. I'll keep doing and updating the pull request.
+  I am so so sorry because I am not evoluting nice.
+   */
 
-  function main(url) {
+
+  function main(url) {}
     fetchJSON(url, (err, repos) => {
       const root = document.getElementById('root');
       if (err) {
@@ -47,8 +68,8 @@
       repos.forEach(repo => renderRepoDetails(repo, ul));
     });
   }
-
+ 
   const HYF_REPOS_URL =
-    'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
+    'https://api.github.com/orgs/hackyourfuture/repos?per_page=10';
   window.onload = () => main(HYF_REPOS_URL);
 }
